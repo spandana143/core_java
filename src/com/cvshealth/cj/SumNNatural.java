aac027dfe17461d77e28d32c9b27bd0765951a67
Spandana <spandanakollipara@gmail.com>
2021-07-08 21:21:06 -0400

    Added sum of Natural Numbers examples...

---
+++ src/com/cvshealth/cj/SumNNatural.java
@@ -0,0 +1,11 @@
+package com.cvshealth.cj;
+
+public class SumNNatural {
+
+	public static void main(String[] args) {
+		int N = 10;
+		int sum = N * N + 1 / 2;
+		System.out.println("The sum is : "+sum);
+	}
+
+}
